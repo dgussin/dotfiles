2cf4104adf1fe96f0700d7508143e603ea2c8844
Darren Gussin <186234+dgussin@users.noreply.github.com>
2021-12-27 13:26:52 -0500

    Install Alfred
    Use --no-quarantine option (for other casks too)

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -7,5 +7,7 @@ echo "\n<<< Starting Howebrew Setup >>>\n"
 brew install httpie
 brew install bat
 
-brew install google-chrome
-brew install visual-studio-code
+# casks
+brew install --no-quarantine google-chrome
+brew install --no-quarantine visual-studio-code
+brew install --no-quarantine alfred
