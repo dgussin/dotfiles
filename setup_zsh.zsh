4e0ab2c2a1cba291fc8da4f4b8b2adee37846edf
Darren Gussin <186234+dgussin@users.noreply.github.com>
2022-05-30 22:34:17 -0400

    Redirecting standard output to the blackhole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation unnecessary; int's in the Brewfile.
 
 echo "Enter superuser (sudo) passwrod to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/opt/homebrew/bin/zsh'
