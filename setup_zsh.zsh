37e8dd7120ba4c97c3430203f729b64e403e2e7f
Darren Gussin <186234+dgussin@users.noreply.github.com>
2022-06-06 23:19:45 -0400

    prevent multiple execution of commands

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,6 +4,19 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; int's in the Brewfile.
 
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) passwrod to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
+
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
 echo "Enter superuser (sudo) passwrod to edit /etc/shells"
 echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
